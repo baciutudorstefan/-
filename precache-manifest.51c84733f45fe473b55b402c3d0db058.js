36f49207567bef98e7678f3623562a132357542b
BaciuTudor <baciu.tudorstefan@gmail.com>
2020-10-12 22:27:43 +0300

    Updates

---
+++ precache-manifest.51c84733f45fe473b55b402c3d0db058.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa29b1ba3b720ebd27068f38c43d9fc7",
+    "url": "/-/index.html"
+  },
+  {
+    "revision": "fdbd9c566ffee66d5143",
+    "url": "/-/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9beefb6a48e3075db653",
+    "url": "/-/static/css/main.b58d87b9.chunk.css"
+  },
+  {
+    "revision": "fdbd9c566ffee66d5143",
+    "url": "/-/static/js/2.2e4b5c47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-/static/js/2.2e4b5c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9beefb6a48e3075db653",
+    "url": "/-/static/js/main.61e9858d.chunk.js"
+  },
+  {
+    "revision": "cd1bc51de83fa9cb5269",
+    "url": "/-/static/js/runtime-main.6a8fe359.js"
+  }
+]);
